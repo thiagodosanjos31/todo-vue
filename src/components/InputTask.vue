ce304a0466a56cc907acfa5beebc0395c569445c
Thiago dos Anjos <thiagoadosanjos@outlook.com.br>
2019-10-21 13:00:35 -0300

    Inicializando projeto no repositório

---
+++ src/components/InputTask.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+      <input placeholder="O que precisa ser feito?" class="new-todo">
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
